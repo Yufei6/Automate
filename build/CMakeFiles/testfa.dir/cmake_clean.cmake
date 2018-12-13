26f23ed1474bd455b9e7e2dd99efda7b4712b32b
Yufei6 <m.liuyufei@qq.com>
2018-12-13 10:38:27 +0100

    Add TestScanetteCorrige

---
+++ build/CMakeFiles/testfa.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testfa.dir/testfa.cc.o"
+  "CMakeFiles/testfa.dir/Automaton.cc.o"
+  "CMakeFiles/testfa.dir/googletest/googletest/src/gtest-all.cc.o"
+  "testfa.pdb"
+  "testfa"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testfa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
